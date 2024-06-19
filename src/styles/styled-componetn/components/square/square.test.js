37b0f485058ba737414a5db60aad97825c839a56
Jose-Lucass <joselucas3005@gmail.com>
2024-06-18 22:06:05 -0300

    Feat: created test jest

---
+++ src/styles/styled-componetn/components/square/square.test.js
@@ -0,0 +1,17 @@
+v     
+
+import React from "react";
+import { render } from "@testing-library/react";
+import Square from ".";
+
+describe('vou realizar alguns testes com o square', () =>{
+    it('verifica se o width do square tem 100px ', () =>{
+        const element = render(<Square />)
+
+        expect(element).toHaveStyle('width: 100px')
+    })
+})
+
+export default function(){
+
+}
